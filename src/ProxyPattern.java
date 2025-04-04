eeffe6a93f7160fe1c8ee67464d5fc239a0a136d
SatimAskhat <s23067494@narxoz.kz>
2025-04-04 13:51:07 +0500

    Update ProxyPattern.java

--- src/ProxyPattern.java
+++ src/ProxyPattern.java
@@ -1,1 +1,17 @@
+public class ProxyPatternDemo {
+    public static void main(String[] args) {
+        Image image = new ImageProxy("house1.jpg");
+
+        image.displayThumbnail(); 
+        image.displayFullImage(); 
+
+        System.out.println("---");
+
+        AgentImageUploader agent1 = new AgentImageUploader(true);
+        agent1.uploadImage("villa.png");
+
+        AgentImageUploader guest = new AgentImageUploader(false);
+        guest.uploadImage("fake.png");
+    }
+}
 
